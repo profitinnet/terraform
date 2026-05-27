cd2908d91947099abfb111747e7a377fd3ea747e
legrey <legrey@i.ua>
2026-05-27 15:57:54 +0300

    feat: move state to s3

---
+++ main.tf
@@ -0,0 +1,43 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.92"
+    }
+  }
+
+  required_version = ">= 1.2"
+
+  backend "s3" {
+    bucket       = "legrey-terraform-bucket"
+    key          = "global/s3/terraform.tfstate"
+    region       = "eu-central-1"
+    encrypt      = true
+    use_lockfile = true
+  }
+
+}
+
+provider "aws" {
+  region = "eu-central-1"
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-resolute-26.04-amd64-server-*"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "app_server" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "learn-terraform"
+  }
+}
