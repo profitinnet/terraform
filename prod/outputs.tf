9a99bf76990e4001bb39d563f8189c89b113da73
devopslegrey <devopslegrey@gmail.com>
2026-06-07 23:42:51 +0300

    Fix EC2 outputs

--- prod/outputs.tf
+++ prod/outputs.tf
@@ -1,3 +1,11 @@
 output "app_server_public_ip" {
   value = aws_instance.app_server.public_ip
 }
+
+output "app_server_id" {
+  value = aws_instance.app_server.id
+}
+
+output "app_server_public_dns" {
+  value = aws_instance.app_server.public_dns
+}
